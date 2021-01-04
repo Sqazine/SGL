c634827416c56788d0845e8ff28eb447ee78b945
Sqazine <51524449+Sqazine@users.noreply.github.com>
2021-01-05 01:26:29 +0800

    add function delarations

--- Example/ecs/entity/Entity.h
+++ Example/ecs/entity/Entity.h
@@ -1,12 +1,31 @@
 #pragma once
 #include <string>
 #include <memory>
+#include <vector>
+#include "ecs/component/Component.h"
 class Entity
 {
 public:
-    
+    Entity();
+    Entity(const std::string &name);
+
+    void AddComponent(const std::shared_ptr<Component> &component);
+
+    template <typename T, typename... Args>
+    void AddComponent(Args &&...params);
+
+    template <typename T>
+    void RemoveComponent();
+
+    void ClearComponents();
+
+    template <typename T>
+    const std::shared_ptr<T> &GetComponent();
+
+    void SetName();
+    const std::string& GetName();
 protected:
     std::string m_Name;
     std::weak_ptr<class Scene> m_Owner;
-    std::unique_ptr<>
+    std::vector<std::shared_ptr<class Component>> m_Components;
 };
--- Example/scene/Scene.h
+++ Example/scene/Scene.h
@@ -1,6 +1,16 @@
 #pragma once
-
+#include <string>
+#include <vector>
+#include <memory>
 class Scene
 {
+public:
+    Scene();
+    Scene(const std::string& name);
 
+    void SetName();
+    const std::string& GetName();
+private:
+    std::string m_Name;
+    std::vector<std::shared_ptr<class Entity>> m_Entities;
 };
